4ee56fc0e8d1b606f2033df846283cefa0df14de
EldyAG <eldyfirmansyah84@gmail.com>
2024-03-08 09:27:54 -0800

    first commit

---
+++ kuliah/pertemuan5/latihan2.php
@@ -0,0 +1,43 @@
+<?php
+$binatang = ["😼", "🐰", "🐵", "🐨", "🐮", "💩"];
+$makanan = ["🍕", "🍔", "🍜", "🍝", "🍗", "🥪"]
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Array</title>
+</head>
+<body>
+    <h3>Daftar Binatang</h3>
+    <ol>
+        <?php for($i = 0; $i < count($binatang); $i++) { ?>
+        <li><?php echo $binatang[$i] ?></li>
+        <?php } ?>
+    </ol>
+    
+    <h3>Daftar Makanan</h3>
+    <ul>
+        <?php for($i = 0; $i < count($makanan); $i++) { ?>
+            <li><?php echo $makanan[$i] ?></li>
+        <?php } ?>
+    </ul>
+
+    <hr>
+
+    <h3>Daftar Binatang</h3>
+    <ol>
+        <?php foreach ($binatang as $b) : ?>
+            <li><?php echo $b; ?></li>
+        <?php endforeach ?>
+    </ol>
+
+    <h3>Daftar Makanan</h3>
+    <ol>
+        <?php foreach ($makanan as $m) : ?>
+            <li><?php echo $m; ?></li>
+        <?php endforeach ?>
+    </ol>
+</body>
+</html>
